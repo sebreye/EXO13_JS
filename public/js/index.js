862cb1e1d8405b98db5696cba5e21f45c91a7d9e
sebastian Reyes <sebreyes222gmail.com>
2022-12-22 09:50:49 +0100

    ajout exo 1 et 2

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+let tab = ["sebastian", "brandon", "mohamed", "denis", "charlotte", "yassine", "quentin", "idrisse","victor", "vitor", "laurent", "enide", "fadia", "adil", "shahin"]
+
+for ( let i = 0; i<tab.length; i++){
+    alert(`bonjour ${tab[i]}`)
+}
+
+let nb = prompt("entrez un nombre :")
+
+for (let i = 0; i <= nb; i++) {
+
+    console.log(i);
+}
